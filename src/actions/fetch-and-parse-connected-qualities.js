8b2b1fa1c17d5fd52de586cbf89d06ce7d562492
sdob <sdob@cs.ucc.ie>
2017-09-05 10:45:12 +0100

    Add comments

--- src/actions/fetch-and-parse-connected-qualities.js
+++ src/actions/fetch-and-parse-connected-qualities.js
@@ -14,16 +14,21 @@ function fetchConnectedQualities() {
 
 
 function parseConnectedQualities(response) {
+  // JQuery lets us find elements by children's IDs
   const $el = $(response);
 
+  // Get the Renown and Favours values for each faction
   const renown = findMatches(renownIDs);
   const favours = findMatches(favourIDs);
   return { renown, favours };
 
   function findMatches(ids) {
     return Object.keys(factions).reduce((acc, faction) => {
+      // Find the favour or renown ID corresponding to this faction
       const id = ids[factions[faction]];
+      // Find the text of the corresponding element
       const text = $el.children().has(`div#infoBarQImage${id}`).text();
+      // Search for a number
       const match = /[^0-9]*([0-9]+)/.exec(text);
       if (match) {
         return {...acc, [factions[faction]]: Number(match[1])};
