22732d31e07ab7f9613322485dff89407d4c7d96
sdob <sdob@cs.ucc.ie>
2017-09-05 12:38:17 +0100

    Create 'preferences' module

--- src/popup/CategoryOption.jsx
+++ src/popup/CategoryOption.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { setCategoryVisibility } from './actions';
+import { setCategoryVisibility } from '../preferences/actions';
 
 class CategoryOption extends Component {
   constructor() {
