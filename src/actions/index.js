202c35082b8f0c64eee0ad6c7f9aab19abbc8739
sdob <sdob@cs.ucc.ie>
2017-09-05 10:47:53 +0100

    Create category actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,6 @@
 import fetchAndParseConnectedQualities from './fetch-and-parse-connected-qualities';
 
-const preferences = {
-  CATEGORY_EXPANSION: 'preferences/category-expansion',
-};
-
 const FAVOURS_AND_RENOWN = 'FAVOURS_AND_RENOWN';
-
-function setExpanded({ category, expanded }) {
-  return (dispatch) => {
-    dispatch({ type: preferences.CATEGORY_EXPANSION, payload: { category, expanded } });
-  };
-}
 
 function fetchConnectedQualities() {
   return (dispatch) => {
@@ -26,6 +16,5 @@ function fetchConnectedQualities() {
 
 export {
   preferences,
-  setExpanded,
   fetchConnectedQualities,
 };
---
+++ src/categories/actions.js
@@ -0,0 +1,11 @@
+const CATEGORY_EXPANSION = 'preferences/category-expansion';
+
+function setExpanded({ category, expanded }) {
+  return (dispatch) => {
+    dispatch({ type: CATEGORY_EXPANSION, payload: { category, expanded } });
+  };
+}
+
+export {
+  setExpanded,
+};
