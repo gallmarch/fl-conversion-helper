ac510d8a5336c0098ef12bb062efd0c2a04b0201
sdob <sdob@cs.ucc.ie>
2017-09-22 14:07:21 +0100

    Fix isDisabled bug

--- src/items/UsableItem.jsx
+++ src/items/UsableItem.jsx
@@ -1,5 +1,6 @@
-/* eslint-disable jsx-a11y/href-no-hash */
-// Fallen London uses <a href="#"> for its clickable items too
+/* eslint-disable jsx-a11y/href-no-hash jsx-a11y/no-static-element-interactions */
+// We're not going to try to fix the accessibility issues, so just disable the
+// ESLint warnings
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
@@ -11,7 +12,7 @@ export default function UsableItem(props) {
   return (
     <li>
       <a
-        className={classNames('tooltip', 'usableitem', { disabled: 'isDisabled' })}
+        className={classNames('tooltip', 'usableitem', { disabled: isDisabled })}
         onClick={() => inventoryMatch.parentNode.click()}
       >
         <div className="qq">{quantity}</div>
@@ -31,10 +32,12 @@ UsableItem.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
   ]),
   inventoryMatch: validateDOMElement.isRequired,
+  isDisabled: PropTypes.bool,
   quantity: PropTypes.number.isRequired,
 };
 
 UsableItem.defaultProps = {
   children: null,
+  isDisabled: false,
 };
 
