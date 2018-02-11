5201cac3884a679e72667518b36121ba184d7c32
sdob <sdob@cs.ucc.ie>
2018-02-11 14:20:39 +0000

    Lint: disable import/prefer-default-export

--- src/preferences/utils.js
+++ src/preferences/utils.js
@@ -1,10 +1,11 @@
+/* eslint-disable import/prefer-default-export */
 import { DEFAULT_PREFERENCES, PREFERENCES_CHANGED } from './constants';
 
 // Loading preferences kicks everything off; if we have pre-existing
 // preferences, then we'll dispatch an action to tell the UI what it
 // should display; if not, then we are probably at first run, in which
 // case we'll populate the preferences with our defaults.
-function loadPreferences({ store, storage }) {
+export function loadPreferences({ store, storage }) {
   // Retrieve preferences
   storage.get(null, ({ preferences }) => {
     // Check whether we have stored preferences. If not, then storage
@@ -12,14 +13,11 @@ function loadPreferences({ store, storage }) {
     // should trigger a storage changed event)
     if (preferences === undefined) {
       const defaultPreferences = DEFAULT_PREFERENCES;
-      return storage.set({ preferences: defaultPreferences });
+      storage.set({ preferences: defaultPreferences });
+      return;
     }
     // If we have preferences, dispatch an action so that the UI knows
     // how to present itself
     store.dispatch({ type: PREFERENCES_CHANGED, payload: preferences });
   });
 }
-
-export {
-  loadPreferences,
-};
