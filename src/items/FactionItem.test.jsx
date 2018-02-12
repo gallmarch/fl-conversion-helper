1c50ba91053c474f603687aaf77ab9900d18ddf0
sdob <sdob@cs.ucc.ie>
2018-02-12 12:31:31 +0000

    Add basic FactionItem test

--- src/items/FactionItem.test.jsx
+++ src/items/FactionItem.test.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import FactionItem from './FactionItem';
+import { FactionItem } from './FactionItem';
 
 describe('FactionItem', () => {
   describe('when the user has no matching item', () => {
     it('matches its snapshot', () => {
       const props = {
         attributes: { key: 0 },
-        enablementPreferences: 0,
+        enablementPreference: 0,
         factions: { favours: {}, renown: {} },
         id: 'some-component-id',
       };
