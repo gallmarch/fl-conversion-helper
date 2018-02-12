e701339e393ecf8f9b470ad83684fa4e4ca53450
sdob <sdob@cs.ucc.ie>
2018-02-12 12:27:23 +0000

    Tweak Eslint: Jest, ++ in for loops

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
   "extends": "airbnb",
+  "plugins": ["jest"],
   "rules": {
+    "no-plusplus": ['error', { 'allowForLoopAfterthoughts': true }],
     "no-use-before-define": ["error", { "functions": false, "classes": true }],
   },
   "env": {
     "browser": true,
+    "jest/globals": true,
     "webextensions": true,
   },
 };
