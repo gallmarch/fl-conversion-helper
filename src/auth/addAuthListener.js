945ef7f850a884221bccc6f09f30d80d1eb09ab7
sdob <sdob@cs.ucc.ie>
2018-05-31 17:02:38 +0100

    Remove all quotes from access token

--- src/auth/addAuthListener.js
+++ src/auth/addAuthListener.js
@@ -26,5 +26,5 @@ function unsetAuthHeader() {
 }
 
 function setAuthHeader({ access_token }) {
-  axios.defaults.headers.common.Authorization = `Bearer ${access_token.replace('"', '')}`;
+  axios.defaults.headers.common.Authorization = `Bearer ${access_token.replace(/"/g, '')}`;
 }
