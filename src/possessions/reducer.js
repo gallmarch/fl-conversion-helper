9becc3a736dcd0fff707d46734b69a6d927066a6
sdob <sdob@cs.ucc.ie>
2018-09-13 17:34:22 +0100

    Fix even more lint

--- src/possessions/reducer.js
+++ src/possessions/reducer.js
@@ -10,18 +10,21 @@ const INITIAL_STATE = {
   possessions: [],
 };
 
-export default function reducer(state=INITIAL_STATE, action) {
+export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case FETCHING_POSSESSIONS:
       return { ...state, isFetching: true };
+
     case FILTER_STRING_CHANGED:
       return { ...state, filterString: action.payload };
+
     case POSSESSIONS_FETCHED:
       return {
         ...state,
         isFetching: false,
         possessions: action.payload,
       };
+
     default:
       return state;
   }
