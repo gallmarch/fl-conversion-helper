2f3f5a071028a2f301d45c1e01cd32c5862041e3
sdob <sdob@cs.ucc.ie>
2019-01-25 12:01:52 +0000

    Add tests for MenuItem

--- src/menu-items/__tests__/MenuItem.test.jsx
+++ src/menu-items/__tests__/MenuItem.test.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { MenuItem } from '../MenuItem';
+import scrollToComponentByName from '../scrollToComponentByName';
+
+jest.mock('../scrollToComponentByName');
 
 describe('MenuItem', () => {
   it('mounts without crashing', () => {
     const name = 'Category name';
     const visibleItems = {};
     shallow(<MenuItem name={name} items={[]} visibleItems={visibleItems} />);
+  });
+
+  it('scrolls when clicked', () => {
+    const name = 'Category name';
+    const items = [1];
+    const visibleItems = { 1: true };
+    const wrapper = mount(<MenuItem name={name} items={items} visibleItems={visibleItems} />);
+    wrapper.find('button').simulate('click');
+    expect(scrollToComponentByName).toHaveBeenCalled();
   });
 });
