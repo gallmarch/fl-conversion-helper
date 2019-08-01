a35fee21cf1296b7e1137f56d305a44aead64fb0
sdob <sdob@cs.ucc.ie>
2019-08-01 10:31:15 +0100

    Remove logs

--- src/myself/actions/fetchMyself.js
+++ src/myself/actions/fetchMyself.js
@@ -2,7 +2,6 @@ import { FETCH_MYSELF_REQUESTED } from '../types';
 
 export default function fetchMyself() {
   return () => {
-    console.info('Fetching myself');
     chrome.runtime.sendMessage(
       { type: FETCH_MYSELF_REQUESTED },
     );
